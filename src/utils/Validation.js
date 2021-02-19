770537855c4ffa3c70595b3e13a72915e7bef95e
vitalii-grigorash <vitalii.grigorash@yandex.ru>
2021-02-19 11:08:30 +0300

    first commit

---
+++ src/utils/Validation.js
@@ -0,0 +1,14 @@
+  
+import { useState, useCallback } from "react";
+
+export const Validation = () => {
+  const [value, setValue] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isValid, setIsValid] = useState(false);
+  const onChange = useCallback((evt) => {
+    setValue(evt.target.value);
+    setErrorMessage(evt.target.validationMessage);
+    setIsValid(evt.target.validity.valid);
+  }, []);
+  return { value, errorMessage, setErrorMessage, isValid, setValue, onChange };
+};
