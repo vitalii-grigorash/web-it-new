723cd3bc7078ca8e0baebdf6ff37e49e060e3a0c
vitalii-grigorash <vitalii.grigorash@yandex.ru>
2021-04-05 17:29:18 +0300

    added PriceTableIt section

---
+++ src/components/PriceTableIt/PriceTableIt.js
@@ -0,0 +1,324 @@
+import React from 'react';
+
+function PriceTableIt () {
+
+    return (
+        <section className="price-table-it">
+            <div className="price-table-it__container">
+                <h2 className="price-table-it__heading">стоимость разовых it-услуг</h2>
+                <div className="it-table">
+                    <div className="it-table__row">
+                        <div className="it-table__left-column it-table__left-column_heading">
+                            <p className="it-table__text it-table__text_main">Вид услуги</p>
+                        </div>
+                        <div className="it-table__right-column it-table__right-column_heading-main">
+                            <p className="it-table__text it-table__text_main">Стоимость услуги, руб.</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column it-table__left-column_heading">
+                            <p className="it-table__text">Серверы</p>
+                        </div>
+                        <div className="it-table__right-column it-table__right-column_heading"></div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Внедрение Сервера Windows под ключ: установка, настройка, настройка
+                                трёх любых служб, настройка Active Directory (и добавление до 15
+                                пользователей), настройка firewall (файрвола) и антивируса
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 6000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Внедрение Сервера Linux под ключ: установка, настройка, настройка
+                                трёх любых служб, настройка Active Directory (и добавление до 15
+                                пользователей), настройка firewall (файрвола) и антивируса
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 6500</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Внедрение систем хранения данных (СХД)
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 1000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column it-table__left-column_heading">
+                            <p className="it-table__text">Организация серверов удаленного рабочего стола</p>
+                        </div>
+                        <div className="it-table__right-column it-table__right-column_heading"></div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Организация серверов удаленного рабочего стола
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 1000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column it-table__left-column_heading">
+                            <p className="it-table__text">Настройка резервного копирования данных</p>
+                        </div>
+                        <div className="it-table__right-column it-table__right-column_heading"></div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Настройка резервного копирования данных
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 1200</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column it-table__left-column_heading">
+                            <p className="it-table__text">АТС и телефония</p>
+                        </div>
+                        <div className="it-table__right-column it-table__right-column_heading"></div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Развертывание офисной телефонии
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 2000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column it-table__left-column_heading">
+                            <p className="it-table__text">Оснащение переговорных комнат и конференций</p>
+                        </div>
+                        <div className="it-table__right-column it-table__right-column_heading"></div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Техническое оснащение конференц-залов
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 12000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column it-table__left-column_heading">
+                            <p className="it-table__text">Установка и развертывание рабочих мест</p>
+                        </div>
+                        <div className="it-table__right-column it-table__right-column_heading"></div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Установка и развертывание рабочих мест
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 3000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column it-table__left-column_heading">
+                            <p className="it-table__text">Внедрение систем корпоративной печати</p>
+                        </div>
+                        <div className="it-table__right-column it-table__right-column_heading"></div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Внедрение систем корпоративной печати
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 1000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column it-table__left-column_heading">
+                            <p className="it-table__text">Внедрение систем виртуализации</p>
+                        </div>
+                        <div className="it-table__right-column it-table__right-column_heading"></div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Составление технического задания
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 11000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Установка гипервизора
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 2000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Виртуализация одной физической машины
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 6000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Развертывание новой чистой виртуальной машины
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 5000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Развертывание одной роли на сервере
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 1500</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Развертывание Exchange на 100 пользователей
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 120000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Развертывание Zabbix с агентом и внедренеи preset’ов
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 60000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Развертывание SQL на 100 пользователей
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">от 60000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column it-table__left-column_heading">
+                            <p className="it-table__text">Переезд офиса</p>
+                        </div>
+                        <div className="it-table__right-column it-table__right-column_heading-main">
+                            <p className="it-table__text">от 8000</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Консультация специалиста по подготовке
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">уточняйте</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Сохранение всех критических данных
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">уточняйте</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Демонтаж офисного оборудования
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">уточняйте</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Правильная упаковка
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">уточняйте</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Контроль перевозки
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">уточняйте</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Сборка оборудования на новом месте
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">уточняйте</p>
+                        </div>
+                    </div>
+                    <div className="it-table__row">
+                        <div className="it-table__left-column">
+                            <p className="it-table__text">
+                                Создание условий работы на новом месте
+                                </p>
+                        </div>
+                        <div className="it-table__right-column">
+                            <p className="it-table__text">уточняйте</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default PriceTableIt;
