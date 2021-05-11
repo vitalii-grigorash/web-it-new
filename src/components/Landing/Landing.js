26054b4bdc7ad8019840a724f3ba3bf8c7bf1e9c
vitalii-grigorash <vitalii.grigorash@yandex.ru>
2021-05-11 14:11:08 +0300

    added PriceTableLanding component

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,31 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import mouse from '../../images/svg/mouse.svg';
+import PriceTableLanding from '../PriceTableLanding/PriceTableLanding';
 
-function Landing(props) {
+function Landing (props) {
 
     const {
         onOpenPopup,
     } = props;
 
-    const [isFirstActive, setFirstActive] = useState(true);
-    function handleFirstSectionActive () {
-        setFirstActive(true);
-        setSecondActive(false);
-        setLastActive(false);
-    }
-    const [isSecondActive, setSecondActive] = useState(false);
-    function handleSecondSectionActive () {
-        setFirstActive(false);
-        setLastActive(false);
-        setSecondActive(true);
-    }
-    const [isLastActive, setLastActive] = useState(false);
-    function handleLastSectionActive () {
-        setFirstActive(false);
-        setSecondActive(false);
-        setLastActive(true);
-    }
     return (
         <div className="landing">
             <Helmet
@@ -85,94 +68,7 @@ function Landing(props) {
                     </p>
                 </div>
             </section>
-            <section className="price-table">
-                <div className="price-table__container">
-                    <div className="price-table__section">
-                        <div className={`price-table__heading-container-first ${isFirstActive && 'price-table__heading-container-first_active'}`} onClick={handleFirstSectionActive}>
-                            <h5 
-                                className={`price-table__heading ${isFirstActive && 'price-table__heading_active'}`}
-                            >
-                                LANDING PAGE "БАЗОВЫЙ" (14 дней) 
-                                <span 
-                                    className={`price-table__heading-span ${isFirstActive && 'price-table__heading-span_active'}`}
-                                > 45 000 руб.</span>
-                            </h5>
-                        </div>
-                        <ul className={`price-table__list ${isFirstActive && 'price-table__list_active'}`}>
-                            <li className={`price-table__list-item ${isFirstActive && 'price-table__list-item_active'}`}>Объём лендинга 5-7 блоков</li>
-                            <li className={`price-table__list-item ${isFirstActive && 'price-table__list-item_active'}`}>Анализ ниши</li>
-                            <li className={`price-table__list-item ${isFirstActive && 'price-table__list-item_active'}`}>Составление технического задания</li>
-                            <li className={`price-table__list-item ${isFirstActive && 'price-table__list-item_active'}`}>Разработка уникального дизайна</li>
-                            <li className={`price-table__list-item ${isFirstActive && 'price-table__list-item_active'}`}>До 3 форм обартной связи</li>
-                            <li className={`price-table__list-item ${isFirstActive && 'price-table__list-item_active'}`}>Наполнение сайта контентом</li>
-                            <li className={`price-table__list-item ${isFirstActive && 'price-table__list-item_active'}`}>Кроссбраузерная верстка (кроме Safari и IE ниже 11 версии)</li>
-                            <li className={`price-table__list-item ${isFirstActive && 'price-table__list-item_active'}`}>Адаптивная (резиновая) верстка под планшеты и мобильные устройства</li>
-                            <li className={`price-table__list-item ${isFirstActive && 'price-table__list-item_active'}`}>Почтовый ящик с доменным именем</li>
-                            <li className={`price-table__list-item ${isFirstActive && 'price-table__list-item_active'}`}>Оплата в 2 этапа (предоплата 50%)</li>
-                        </ul>
-                        <p className={`price-table__under-list-price ${isFirstActive && 'price-table__under-list-price_active'}`}>45 000 руб.</p>
-                    </div>
-                    <div className="price-table__section">
-                        <div className={`price-table__heading-container-second ${isSecondActive && 'price-table__heading-container-second_active'}`} onClick={handleSecondSectionActive}>
-                            <h5 
-                                className={`price-table__heading ${isSecondActive && 'price-table__heading_active'}`}
-                            >
-                                LANDING PAGE "РАСШИРЕННЫЙ" (17&nbsp;дней)
-                                <span 
-                                    className={`price-table__heading-span ${isSecondActive && 'price-table__heading-span_active'}`}
-                                > 60 000 руб.</span>
-                            </h5>
-                        </div>
-                        <ul className={`price-table__list ${isSecondActive && 'price-table__list_active'}`}>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Объём лендинга 7-9 блоков</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Анализ ниши</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Составление технического задания</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Разработка уникального дизайна</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>До 5 форм обартной связи</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Наполнение сайта контентом</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Кроссбраузерная верстка (кроме Safari и IE ниже 11 версии)</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Личный руководитель проекта</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Возможность использовать эффекты и анимацию в том числе</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Почтовый ящик с доменным именем</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Адаптивная (резиновая) верстка под планшеты и мобильные устройства</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Разработка логотипа</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Установка Я.Метрики и G.Analytics</li>
-                            <li className={`price-table__list-item ${isSecondActive && 'price-table__list-item_active'}`}>Оплата в 2 этапа (предоплата 50%)</li>
-                        </ul>
-                        <p className={`price-table__under-list-price ${isSecondActive && 'price-table__under-list-price_active'}`}>60 000 руб.</p>
-                    </div>
-                    <div className="price-table__section">
-                        <div className={`price-table__heading-container-last ${isLastActive && 'price-table__heading-container-last_active'}`} onClick={handleLastSectionActive}>
-                            <h5 
-                                className={`price-table__heading ${isLastActive && 'price-table__heading_active'}`}
-                            >
-                                LANDING PAGE "МАКСИМАЛЬНЫЙ" (28 дней) 
-                                <span 
-                                    className={`price-table__heading-span ${isLastActive && 'price-table__heading-span_active'}`}
-                                > 75 000 руб.</span>
-                            </h5>
-                        </div>
-                        <ul className={`price-table__list ${isLastActive && 'price-table__list_active'}`}>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Объём лендинга 9-12 блоков</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Анализ ниши</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Составление технического задания</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Разработка уникального дизайна</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>До 10 форм обартной связи</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Наполнение сайта контентом</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Кроссбраузерная верстка (кроме Safari и IE ниже 11 версии)</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Личный руководитель проекта</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Возможность использовать эффекты и анимацию в том числе</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Почтовый ящик с доменным именем</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Адаптивная (резиновая) верстка под планшеты и мобильные устройства</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Разработка логотипа</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Установка Я.Метрики и G.Analytics</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Установка и настройка SSL-сертификата</li>
-                            <li className={`price-table__list-item ${isLastActive && 'price-table__list-item_active'}`}>Оплата в 2 этапа (предоплата 50%)</li>
-                        </ul>
-                        <p className={`price-table__under-list-price ${isLastActive && 'price-table__under-list-price_active'}`}>75 000 руб.</p>
-                    </div>
-                </div>
-            </section>
+            <PriceTableLanding />
         </div>
     );
 }
--- src/components/PriceWeb/PriceWeb.js
+++ src/components/PriceWeb/PriceWeb.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Helmet } from 'react-helmet-async';
 import mouse from '../../images/svg/mouse.svg';
+import PriceTableLanding from '../PriceTableLanding/PriceTableLanding';
 
 function PriceWeb (props) {
 
     const {
         onOpenPopup,
     } = props;
+
+    const [isFirstActive, setFirstActive] = useState(true);
+    function handleFirstSectionActive () {
+        setSecondActive(false);
+        setThirdActive(false);
+        setFourthActive(false);
+        setFirstActive(true);
+    };
+
+    const [isSecondActive, setSecondActive] = useState(false);
+    function handleSecondSectionActive () {
+        setFirstActive(false);
+        setThirdActive(false);
+        setFourthActive(false);
+        setSecondActive(true);
+    };
+
+    const [isThirdActive, setThirdActive] = useState(false);
+    function handleThirdSectionActive () {
+        setFirstActive(false);
+        setSecondActive(false);
+        setFourthActive(false);
+        setThirdActive(true);
+    };
+
+    const [isFourthActive, setFourthActive] = useState(false);
+    function handleFourthSectionActive () {
+        setFirstActive(false);
+        setSecondActive(false);
+        setThirdActive(false);
+        setFourthActive(true);
+    };
 
     return (
         <div className="price-web">
@@ -28,6 +61,74 @@ function PriceWeb (props) {
                     <p className="price-web__text">Прокрутите вниз</p>
                 </div>
             </section>
+            <h3 className="price-web__tables-heading">СТОИМОСТЬ УСЛУГ</h3>
+            <h4 className="price-web__table-heading">ИНТЕРНЕТ-МАГАЗИН</h4>
+            <section className="price">
+                <div className="price__main-container price__main-container_web">
+                    <div className="price__cards-container">
+                        <div className={`${isFirstActive ? 'card' : 'card-disabled'}`} onClick={handleFirstSectionActive}>
+                            <h5 className={`${isFirstActive ? 'card__heading' : 'card__heading-disabled'}`}>до 100 позиций</h5>
+                            <ul className={`${isFirstActive ? 'card__list' : 'card__list-disabled'}`}>
+                                <li className="card__item">CMS WordPress</li>
+                                <li className="card__item">Базовое SEO</li>
+                                <li className="card__item">Оптимизация под мобильные устройства</li>
+                                <li className="card__item">Весь необходимый функционал</li>
+                                <li className="card__item">Возможность расширения до 500 позиций</li>
+                                <li className="card__item">Оплата on-line</li>
+                                <li className="card__item">Разработка до 45 рабочих дней</li>
+                                <li className="card__item">Дополнительно интеграция с 1с</li>
+                            </ul>
+                            <p className={`${isFirstActive ? 'card__value' : 'card__value-disabled'}`}>от 65 000 рублей</p>
+                        </div>
+                        <div className={`${isSecondActive ? 'card' : 'card-disabled'}`} onClick={handleSecondSectionActive}>
+                            <h5 className={`${isSecondActive ? 'card__heading' : 'card__heading-disabled'}`}>до 500 позиций</h5>
+                            <ul className={`${isSecondActive ? 'card__list' : 'card__list-disabled'}`}>
+                                <li className="card__item">CMS WordPress/Open Cart</li>
+                                <li className="card__item">Базовое SEO</li>
+                                <li className="card__item">Оптимизация под мобильные устройства</li>
+                                <li className="card__item">Весь необходимый функционал</li>
+                                <li className="card__item">Возможность расширения до 1000 позиций</li>
+                                <li className="card__item">Оплата on-line</li>
+                                <li className="card__item">Разработка до 60 рабочих дней</li>
+                                <li className="card__item">Дополнительно интеграция с 1с</li>
+                            </ul>
+                            <p className={`${isSecondActive ? 'card__value' : 'card__value-disabled'}`}>от 75 000 рублей</p>
+                        </div>
+                    </div>
+                    <div className="price__cards-container">
+                        <div className={`${isThirdActive ? 'card' : 'card-disabled'}`} onClick={handleThirdSectionActive}>
+                            <h5 className={`${isThirdActive ? 'card__heading' : 'card__heading-disabled'}`}>до 1000 позиций</h5>
+                            <ul className={`${isThirdActive ? 'card__list' : 'card__list-disabled'}`}>
+                                <li className="card__item">CMS Битрикс/Open Cart</li>
+                                <li className="card__item">Базовое SEO</li>
+                                <li className="card__item">Оптимизация под мобильные устройства</li>
+                                <li className="card__item">Весь необходимый функционал</li>
+                                <li className="card__item">Возможность расширения до 5000 позиций</li>
+                                <li className="card__item">Оплата on-line</li>
+                                <li className="card__item">Разработка от 60 рабочих дней</li>
+                                <li className="card__item">Интеграция с 1с</li>
+                            </ul>
+                            <p className={`${isThirdActive ? 'card__value' : 'card__value-disabled'}`}>от 105 000 рублей</p>
+                        </div>
+                        <div className={`${isFourthActive ? 'card' : 'card-disabled'}`} onClick={handleFourthSectionActive}>
+                            <h5 className={`${isFourthActive ? 'card__heading' : 'card__heading-disabled'}`}>свыше 1000 позиций</h5>
+                            <ul className={`${isFourthActive ? 'card__list' : 'card__list-disabled'}`}>
+                                <li className="card__item">CMS Битрикс</li>
+                                <li className="card__item">Базовое SEO</li>
+                                <li className="card__item">Оптимизация под мобильные устройства</li>
+                                <li className="card__item">Весь необходимый функционал</li>
+                                <li className="card__item">Возможность расширения более 5000 позиций</li>
+                                <li className="card__item">Оплата on-line</li>
+                                <li className="card__item">Разработка от 60 рабочих дней</li>
+                                <li className="card__item">Интеграция с 1с</li>
+                            </ul>
+                            <p className={`${isFourthActive ? 'card__value' : 'card__value-disabled'}`}>от 135 000 рублей</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <h4 className="price-web__table-heading">LANDING PAGE</h4>
+            <PriceTableLanding />
         </div>
     );
 }
